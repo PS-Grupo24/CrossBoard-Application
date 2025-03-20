cdc3b9c12c3f5c40c013c6b8995cdc5f76d9ed28
48318-LuisReis <A48318@alunos.isel.pt>
2025-03-20 17:03:54 +0000

    Match Service implemented.

--- code/CrossBoard Application/server/src/main/kotlin/service/MatchService.kt
+++ code/CrossBoard Application/server/src/main/kotlin/service/MatchService.kt
@@ -1,5 +1,6 @@
 package service
 
+import domain.Move
 import model.GameType
 import model.MultiPlayerMatch
 import repository.interfaces.MatchRepository
@@ -8,8 +9,36 @@ class MatchService(private val matchRepository: MatchRepository) {
     suspend fun enterMatch(userId: UInt, gameType: GameType): MultiPlayerMatch{
         if (matchRepository.getRunningMatchByUser(userId) != null)
             throw IllegalStateException("User is currently in a running match or Waiting")
+        val waitingMatch = matchRepository.getWaitingMatch(gameType)
+        if (waitingMatch != null){
+            val updatedMatch = MultiPlayerMatch(waitingMatch.board, waitingMatch.id, waitingMatch.player1, userId, waitingMatch.gameType)
+            matchRepository.updateMatch(updatedMatch)
+            return updatedMatch
+        }
         val m = MultiPlayerMatch.startGame(userId, gameType)
         matchRepository.addMatch(m)
         return m
     }
+
+    suspend fun getMatchById(matchId: UInt): MultiPlayerMatch {
+        return matchRepository.getMatchById(matchId) ?: throw IllegalArgumentException("Match not found")
+    }
+
+    suspend fun getMatchByUser(userId: UInt): MultiPlayerMatch {
+        return matchRepository.getRunningMatchByUser(userId) ?: throw IllegalArgumentException("Match not found")
+    }
+
+    suspend fun getWaitingMatch(gameType: GameType): MultiPlayerMatch {
+        return matchRepository.getWaitingMatch(gameType) ?: throw IllegalArgumentException("Match not found")
+    }
+
+    suspend fun playMatch(matchId: UInt, userId: UInt, move: Move): MultiPlayerMatch {
+        val match = getMatchById(matchId)
+        if (match.player1 != userId && match.player2 != userId)
+            throw IllegalArgumentException("Player is not in the match")
+        val updatedMatch = match.play(move)
+        matchRepository.updateMatch(updatedMatch)
+        return updatedMatch
+    }
+
 }
