1afba3d99420da873cfd3beee75f93f076908341
48318-LuisReis <A48318@alunos.isel.pt>
2025-05-14 17:09:06 +0100

    Documentation of the Domain section modules: - Column.kt - Match.kt - MatchState.kt - MatchType.kt - Routing.kt (correction of a space) - Row.kt Beginning of the tests for the Domain sections: - MatchStateTests.kt - MatchTests.kt - MatchTypeTests.kt

--- code/CrossBoard Application/shared/src/commonMain/kotlin/com/crossBoard/domain/Match.kt
+++ code/CrossBoard Application/shared/src/commonMain/kotlin/com/crossBoard/domain/Match.kt
@@ -8,9 +8,10 @@ import com.crossBoard.httpModel.toMoveOutput
 import kotlin.random.Random
 
 /**
- * Interface "Game" represents a game.
- * @property id the id of the game.
- * @property board the board of the game.
+ * Interface "Match" represents a Match.
+ * @property id the id of the match.
+ * @property board the board of the match.
+ * @property state the state of the match.
  * @property getPlayerType Function to get the player type.
  */
 interface Match {
@@ -21,14 +22,16 @@ interface Match {
 }
 
 /**
- * Class "MultiPlayerGame" represents a multiplayer game.
- * @param board the board of the game.
+ * Class "MultiPlayerMatch" represents a multiplayer match.
+ * @param board the board of the match.
  * @param id the id of the match.
+ * @param state the state of the match.
  * @param player1 the first player.
  * @param player2 the second player.
  * @param matchType the type of the match.
+ * @param version the version of the match.
  * @param winner the winner of the match.
- * @return Match the match that was created as a multiplayer game.
+ * @return Match the match that was created as a multiplayer match.
  */
 data class MultiPlayerMatch(
     override val board: Board,
@@ -41,57 +44,46 @@ data class MultiPlayerMatch(
     val winner: Int? = null,
 ): Match {
     companion object{
+
         /**
          * Function startGame responsible to start a game.
          * @param player1 the first player.
          * @param matchType the type of the game.
-         * @return Game the game that was created.
+         * @return MultiPlayerMatch the multiplayer match that was created.
          */
-        fun startGame(player1: Int, matchType: MatchType): MultiPlayerMatch = when(matchType){
-            MatchType.TicTacToe -> {
-                val p1 = Player.random()
-                val board = TicTacToeBoardRun(
-                    initialTicTacToePositions(),
-                    emptyList(),
-                    Player.random(),
-                    p1,
-                    p1.other(),
-                )
-                MultiPlayerMatch(
-                    board,
-                    Random.nextInt(from = 1, Int.MAX_VALUE),
-                    MatchState.WAITING,
-                    player1,
-                    null,
-                    matchType,
-                    1,
-                    null
-                )
+        fun startGame(player1: Int, matchType: MatchType): MultiPlayerMatch {
+            require(player1 > 0) { "player1 must be greater than 0" }
+            require(MatchType.entries.toTypedArray().contains(matchType)) { "matchType must be a valid match type" }
+            return when(matchType) {
+                MatchType.TicTacToe -> {
+                    val p1 = Player.random()
+                    val board = TicTacToeBoardRun(
+                        initialTicTacToePositions(),
+                        emptyList(),
+                        Player.random(),
+                        p1,
+                        p1.other(),
+                    )
+                    MultiPlayerMatch(
+                        board,
+                        Random.nextInt(from = 1, Int.MAX_VALUE),
+                        MatchState.WAITING,
+                        player1,
+                        null,
+                        matchType,
+                        1,
+                        null
+                    )
+                }
             }
         }
     }
-    /**
-     * Function play responsible to play a move on the board.
-     * @param move The move to be made.
-     * @return MultiPlayerGame the game after the move was played.
-     */
-    fun play(move: Move): MultiPlayerMatch {
-        val newBoard = board.play(move)
-        val winnerType = if (newBoard is BoardWin) newBoard.winner else null
-        val player1Type = getPlayerType(player1)
-        val winner = if (winnerType == player1Type) player1 else if (winnerType == player1Type.other()) player2 else null
-        return MultiPlayerMatch(
-            newBoard,
-            id,
-            getMatchStateFromBoard(newBoard),
-            player1,
-            player2,
-            matchType,
-            version + 1,
-            winner = winner,
-        )
-    }
 
+    /**
+     * Function join responsible to a player to join a match.
+     * @param userId2 the second player.
+     * @return MultiPlayerMatch the match after the player joined.
+     */
     fun join(userId2: Int): MultiPlayerMatch {
         require(userId2 > 0) { "userId2 must be greater than 0" }
         require(player2 == null) { "This game is full" }
@@ -102,17 +94,40 @@ data class MultiPlayerMatch(
     }
 
     /**
-     * Function forfeit responsible to forfeit the game.
+     * Function play responsible to play a move on the match.
+     * @param move The move to be made.
+     * @return MultiPlayerMatch the match after the move was played.
+     */
+    fun play(move: Move): MultiPlayerMatch {
+        val newBoard = board.play(move)
+        val winnerType = if (newBoard is BoardWin) newBoard.winner else null
+        val player1Type = getPlayerType(player1)
+        val winner = if (winnerType == player1Type) player1 else if (winnerType == player1Type.other()) player2 else null
+        return MultiPlayerMatch(
+            newBoard,
+            id,
+            getMatchStateFromBoard(player2, newBoard),
+            player1,
+            player2,
+            matchType,
+            version + 1,
+            winner = winner,
+        )
+    }
+
+    /**
+     * Function forfeit responsible to forfeit the match.
      * @param player the player that is forfeiting.
-     * @return MultiPlayerGame the game after the forfeit.
+     * @return MultiPlayerMatch the match after the forfeit.
      */
     fun forfeit(player: Int): MultiPlayerMatch {
+        require(player > 0) { "player must be greater than 0" }
         val playerType = getPlayerType(player)
         val newBoard = board.forfeit(playerType)
         return MultiPlayerMatch(
             newBoard,
             id,
-            getMatchStateFromBoard(newBoard),
+            getMatchStateFromBoard(player2, newBoard),
             player1,
             player2,
             matchType,
@@ -123,22 +138,35 @@ data class MultiPlayerMatch(
 
     /**
      * Function getPlayerType responsible to get the player type.
-     * @param userId the player.
+     * @param userId id of the player.
      * @return Player the player type.
      */
     override fun getPlayerType(userId: Int): Player {
         require(userId == player1 || userId == player2) {"This user is not a player in this match"}
-        return when(userId){
+        return when(userId) {
             player1 -> board.player1
             else -> board.player2
         }
     }
 
+    /**
+     * Function equals responsible to compared MultiplayerMatch objects.
+     * @param other the other object to be compared.
+     * @return Boolean true if the objects are equal, false otherwise.
+     */
     override fun equals(other: Any?) = other is MultiPlayerMatch && id == other.id && other.version == version
-    override fun hashCode(): Int {
-        return id.hashCode() + version.hashCode()
-    }
 
+    /**
+     * Function hashCode responsible to get the hash code of the object.
+     * @return Int the hash code of the object.
+     */
+    override fun hashCode(): Int = id.hashCode() + version.hashCode()
+
+    /**
+     * Function isMyTurn responsible to check if it's the player's turn.
+     * @param userId the id of the player.
+     * @return Boolean true if it's the player's turn, false otherwise.
+     */
     fun isMyTurn(userId: Int): Boolean {
         require(userId > 0) { "userId must be greater than 0" }
         require(userId == player1 || userId == player2) { "User is not in match" }
@@ -147,6 +175,11 @@ data class MultiPlayerMatch(
         return board.turn == myType
     }
 
+    /**
+     * Function otherPlayer responsible to get the other player.
+     * @param userId the id of the player.
+     * @return Int the id of the other player.
+     */
     fun otherPlayer(userId: Int): Int {
         require(userId > 0) { "userId must be greater than 0" }
         require(userId == player1 || userId == player2) { "User is not in match" }
@@ -156,9 +189,11 @@ data class MultiPlayerMatch(
     }
 }
 
-
-
-fun MultiPlayerMatch.toMatchOutput() : MatchOutput {
+/**
+ * Function to convert a MultiPlayerMatch to a MatchOutput.
+ * @return MatchOutput the match output.
+ */
+fun MultiPlayerMatch.toMatchOutput(): MatchOutput {
     val winner = if (board is BoardWin) board.winner else null
     val winnerId = when(state){
         MatchState.WIN -> {
@@ -193,8 +228,13 @@ fun MultiPlayerMatch.toMatchOutput() : MatchOutput {
     )
 }
 
+/**
+ * Function to convert a MultiPlayerMatch to a MatchPlayedOutput.
+ * @return MatchPlayedOutput the match played output.
+ */
 fun MultiPlayerMatch.toPlayedMatch() = MatchPlayedOutput(this.board.moves.last().toMoveOutput(), this.version)
 
+/*
 /**
  * Class "SinglePlayerGame" represents a single player game.
  * @param board the board of the game.
@@ -211,4 +251,4 @@ class SinglePlayerMatch(
 ):
     Match {
     override fun getPlayerType(userId: Int): Player = board.player1
-}
+}*/
