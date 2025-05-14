aac7b5efdf404dcab3c57fd2b75229cf07effb5d
48318-LuisReis <A48318@alunos.isel.pt>
2025-05-14 19:12:00 +0100

    Documentation of the Domain section modules: - Move.kt Beginning of the tests for the Domain sections: - MoveTests.kt

--- code/CrossBoard Application/shared/src/commonMain/kotlin/com/crossBoard/domain/Move.kt
+++ code/CrossBoard Application/shared/src/commonMain/kotlin/com/crossBoard/domain/Move.kt
@@ -3,38 +3,64 @@ package com.crossBoard.domain
 import com.crossBoard.httpModel.MoveOutput
 import com.crossBoard.httpModel.TicTacToeMoveOutput
 
+/**
+ * Interface "Move" represents a move in the game.
+ * @property player the player who made the move.
+ */
 sealed interface Move {
     val player: Player
 }
 
 /**
- * Data class "Move" represents a move in the game.
+ * Data class "TicTacToeMove" represents a move in the game TicTacToe.
  * @param player the player who made the move.
  * @param square the square where the move was made.
+ * @return Move the type of that was move.
  */
-data class TicTacToeMove (override val player: Player, val square: Square) : Move
+data class TicTacToeMove(override val player: Player, val square: Square): Move {
 
+    /**
+     * Function "equals" responsible to compare two TicTacToeMove objects.
+     * @param other the other object to compare.
+     * @return Boolean true if the objects are equal, false otherwise.
+     */
+    override fun equals(other: Any?): Boolean =
+        other is TicTacToeMove && other.player == player && other.square.row.number == square.row.number && other.square.column.symbol == square.column.symbol
 
-fun moveToString(move: Move): String {
-    return when (move) {
+    /**
+     * Function "hashCode" responsible to generate a hash code for the TicTacToeMove object.
+     * @return Int the hash code of the object.
+     */
+    override fun hashCode(): Int = player.hashCode() + square.hashCode()
+}
+
+/**
+ * Function "moveToString" responsible to pass a move information to String.
+ * @param move the move to be converted to String.
+ * @return String the String representation of the move.
+ */
+fun moveToString(move: Move): String = when (move) {
         is TicTacToeMove -> {
             "${move.player},${move.square}"
         }
     }
-}
 
-fun String.toMove(matchType: MatchType): Move {
-    when(matchType) {
+/**
+ * Function "toMove" responsible to convert a String to a Move.
+ * @param matchType the type of the match.
+ * @return Move the Move corresponding to the String and the type of the match.
+ */
+fun String.toMove(matchType: MatchType): Move = when(matchType) {
         MatchType.TicTacToe -> {
             val values = split(",")
-            return TicTacToeMove(values[0].toPlayer(), values[1].toSquare(TicTacToeBoard.BOARD_DIM))
+            TicTacToeMove(values[0].toPlayer(), values[1].toSquare(TicTacToeBoard.BOARD_DIM))
         }
-        else -> throw IllegalArgumentException("Invalid type $matchType")
     }
-}
 
-fun MoveOutput.toMove(): Move {
-    return when (this) {
-        is TicTacToeMoveOutput -> TicTacToeMove(player.toPlayer(), square.toSquare(com.crossBoard.domain.TicTacToeBoard.BOARD_DIM))
+/**
+ * Function "toMove" responsible to convert a MoveOutput to a Move.
+ * @return Move the Move corresponding to the MoveOutput.
+ */
+fun MoveOutput.toMove(): Move = when (this) {
+        is TicTacToeMoveOutput -> TicTacToeMove(player.toPlayer(), square.toSquare(TicTacToeBoard.BOARD_DIM))
     }
-}
