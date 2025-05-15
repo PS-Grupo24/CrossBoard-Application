e8dff389b6664a5997ed4d841ae42a8b87a99f5a
48318-LuisReis <A48318@alunos.isel.pt>
2025-05-15 20:18:53 +0100

    Documentation of the Domain section modules: - TicPosition.kt Beginning of the tests for the Domain sections: - TicPositionTests.kt

--- code/CrossBoard Application/shared/src/commonMain/kotlin/com/crossBoard/domain/TicPosition.kt
+++ code/CrossBoard Application/shared/src/commonMain/kotlin/com/crossBoard/domain/TicPosition.kt
@@ -1,21 +1,34 @@
 package com.crossBoard.domain
 
-
+/**
+ * Interface "Position" represents a position of the square in the game.
+ * @property square the square of the position.
+ */
 interface Position{
     val square: Square
 }
 
 /**
- * Data class "Position" represents a position in the game.
+ * Data class "TicPosition" represents a position in the game of TicTacToe.
  * @param player the player at the position.
  * @param square the square of the position.
+ * @return Position the position of the said square.
  */
 data class TicPosition(val player: Player, override val square: Square) : Position {
-    override fun toString(): String {
-        return "${player},${square}"
-    }
+
+    /**
+     * Function "toString" responsible to convert the TicPosition object to a String.
+     * @return String the String representation of the TicPosition object.
+     */
+    override fun toString(): String = "${player},${square}"
 }
 
+/**
+ * Function "toPosition" responsible to convert a String to a Position.
+ * @param boardDim the dimension of the board.
+ * @param matchType the type of the match.
+ * @return Position the Position corresponding to the String and the type of the match.
+ */
 fun String.toPosition(boardDim: Int, matchType: MatchType): Position {
     val values = split(",")
     return when(matchType){
---
+++ code/CrossBoard Application/shared/src/commonTest/kotlin/TicPositionTests.kt
@@ -0,0 +1,37 @@
+import com.crossBoard.domain.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class TicPositionTests {
+    @Test fun creatingTicPosition() {
+        val ticPosition = TicPosition(Player.BLACK, Square(Row(1,3), Column('a')))
+
+        assertEquals(Player.BLACK.name, ticPosition.player.name)
+        assertEquals(2, ticPosition.square.row.number)
+        assertEquals(1, ticPosition.square.row.index)
+        assertEquals('a', ticPosition.square.column.symbol)
+        assertEquals(0, ticPosition.square.column.index)
+    }
+
+    @Test fun creatingTicPositionString() {
+        val ticPosition = TicPosition(Player.BLACK, Square(Row(2,3), Column('a')))
+
+        val stringTicPosition = ticPosition.toString()
+
+        assertEquals("BLACK,1a", stringTicPosition)
+    }
+
+    @Test fun creatingTicPositionWithValidString() {
+        val stringTicPosition = "BLACK,1a".toPosition(3, MatchType.TicTacToe)
+
+        val ticPosition = TicPosition(Player.BLACK, Square(Row(2,3), Column('a')))
+
+        assertTrue(stringTicPosition is TicPosition)
+        assertEquals(ticPosition.player.name, stringTicPosition.player.name)
+        assertEquals(ticPosition.square.row.number, stringTicPosition.square.row.number)
+        assertEquals(ticPosition.square.row.index, stringTicPosition.square.row.index)
+        assertEquals(ticPosition.square.column.symbol, stringTicPosition.square.column.symbol)
+        assertEquals(ticPosition.square.column.index, stringTicPosition.square.column.index)
+    }
+}
