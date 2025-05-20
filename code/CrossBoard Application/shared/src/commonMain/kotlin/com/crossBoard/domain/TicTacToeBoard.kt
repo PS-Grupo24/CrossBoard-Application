d9daaa7d7d7a152eb9229ab168414bfb2fd1ef85
48318-LuisReis <A48318@alunos.isel.pt>
2025-05-20 18:26:06 +0100

    Documentation of the Domain section modules: - TicTacToeBoard.kt Beginning of the tests for the Domain sections: - TicTacToeBoardTests.kt

--- code/CrossBoard Application/shared/src/commonMain/kotlin/com/crossBoard/domain/TicTacToeBoard.kt
+++ code/CrossBoard Application/shared/src/commonMain/kotlin/com/crossBoard/domain/TicTacToeBoard.kt
@@ -2,17 +2,19 @@ package com.crossBoard.domain
 
 
 /**
- * Data class "TicTacToeBoard" represents a Tic Tac Toe board.
+ * Abstract class "TicTacToeBoard" represents a TicTacToe board.
  * @param positions the list of positions in the board.
  * @param moves the list of moves made in the game.
  * @param turn the player who has the turn to play.
  * @param player1 the first player.
  * @param player2 the second player.
- * @return Board the Tic Tac Toe board.
+ * @return Board the TicTacToe board.
  */
-abstract class TicTacToeBoard
-
-: Board {
+abstract class TicTacToeBoard: Board {
+    //Value representing the positions of the board.
+    abstract override val positions: List<TicPosition>
+    //Value representing the list of moves made in the board.
+    abstract override val moves: List<TicTacToeMove>
 
     companion object{
         //The board dimension and the maximum moves.
@@ -20,33 +22,38 @@ abstract class TicTacToeBoard
         const val MAX_MOVES = BOARD_DIM * BOARD_DIM
     }
 
-    abstract override val positions: List<TicPosition>
-    abstract override val moves: List<TicTacToeMove>
     /**
      * Function get responsible to get the player at a specific square or to verify if it is occupied the square.
      * @param square the square to get the player.
      * @return Player the player at the square.
      */
-    override fun get(square: Square): Player? {
-        return positions.find { it.square.row.number == square.row.number && it.square.column.symbol == square.column.symbol }?.player
-    }
+    override fun get(square: Square): Player? =
+        positions.find { it.square.row.number == square.row.number && it.square.column.symbol == square.column.symbol }?.player
 
-
+    /**
+     * Function "equals" responsible to compare two TicTacToeBoard objects.
+     * @param other the other object to compare.
+     * @return Boolean true if the objects are equal, false otherwise.
+     */
     override fun equals(other: Any?) =
         other is TicTacToeBoard && other.positions == positions
 
-
+    /**
+     * Function "hashCode" responsible to generate a hash code for the TicTacToeBoard object.
+     * @return Int the hash code of the object.
+     */
     override fun hashCode(): Int = positions.hashCode()
 }
 
 /**
  * Data class "TicTacToeBoardRun" represents a Tic Tac Toe board in a running state.
  * @param positions the list of positions in the board.
- * @param moves the list of moves made in the game.
+ * @param moves the list of moves made in the board.
  * @param turn the player who has the turn to play.
  * @param player1 the first player.
  * @param player2 the second player.
- * @return TicTacToeBoard the Tic Tac Toe board in a running state with the information.
+ * @return TicTacToeBoard the TicTacToe board in a running state with the information.
+ * @return BoardRun the TicTacToe board in a running state with the information.
  */
 class TicTacToeBoardRun(
     override val positions: List<TicPosition>,
@@ -54,15 +61,16 @@ class TicTacToeBoardRun(
     override val turn: Player,
     override val player1: Player,
     override val player2: Player,
-) : TicTacToeBoard(), BoardRun {
+): TicTacToeBoard(), BoardRun {
+
     /**
-     * Function play responsible to play a move on the board.
+     * Function "play" responsible to play a move on the board.
      * @param move the move to be done in this play.
      * @return Board the board after the move was played.
      */
     override fun play(move: Move): Board {
-        require(move is TicTacToeMove){"Wrong move format"}
-        require(move.player == turn){"Not this player's turn to play!"}
+        require(move is TicTacToeMove) {"Wrong move type"}
+        require(move.player == turn) {"Not this player's turn to play!"}
         require(get(move.square) == Player.EMPTY) {"This position is not empty!"}
         val newPositions = positions.map {
             if (it.square.row.number == move.square.row.number && it.square.column.symbol == move.square.column.symbol)
@@ -74,7 +82,7 @@ class TicTacToeBoardRun(
             verifyWinner(newPositions, move) ->
                 TicTacToeBoardWin(move.player, newPositions, moves + move, turn.other(), player1, player2)
             moves.size == MAX_MOVES - 1 ->
-                TicTacToeBoardDraw(newPositions, moves + move, turn.other(),player1, player2)
+                TicTacToeBoardDraw(newPositions, moves + move, turn.other(), player1, player2)
             else ->
                 TicTacToeBoardRun(newPositions, moves + move, turn.other(), player1, player2)
         }
@@ -94,7 +102,7 @@ class TicTacToeBoardRun(
      * @return Boolean true if there is a winner, false otherwise.
      */
     private fun verifyWinner(positions: List<TicPosition>, move: Move): Boolean {
-        require(move is TicTacToeMove){"Wrong type of move!"}
+        require(move is TicTacToeMove) {"Wrong type of move!"}
         val playerPositions = positions.filter { it.player == move.player}
         return playerPositions.count{it.square.column.symbol == move.square.column.symbol} == BOARD_DIM
                 || playerPositions.count{it.square.row.number == move.square.row.number} == BOARD_DIM
@@ -112,6 +120,7 @@ class TicTacToeBoardRun(
  * @param player1 the first player.
  * @param player2 the second player.
  * @return TicTacToeBoard the Tic Tac Toe board in a win state with the information.
+ * @return BoardWin the Tic Tac Toe board in a win state with the information.
  */
 class TicTacToeBoardWin(
     override val winner: Player,
@@ -120,26 +129,21 @@ class TicTacToeBoardWin(
     override val turn: Player,
     override val player1: Player,
     override val player2: Player,
-) : TicTacToeBoard(), BoardWin {
+): TicTacToeBoard(), BoardWin {
+
     /**
      * Function play responsible to play a move on the board.
-     * @param player the player that is playing.
-     * @param row the row of the move.
-     * @param column the column of the move.
+     * @param move the move to be done in this play.
      * @return Board the board after the move was played.
      */
-    override fun play(move: Move): Board {
-        throw IllegalStateException("The player $winner already won the game.")
-    }
+    override fun play(move: Move): Board = throw IllegalStateException("The player $winner already won the game.")
 
     /**
      * Function forfeit responsible to forfeit the game board.
      * @param player the player that is forfeiting.
      * @return Board the game after the player forfeited.
      */
-    override fun forfeit(player: Player): Board {
-        throw IllegalStateException("Can not forfeit an already finished game!")
-    }
+    override fun forfeit(player: Player): Board = throw IllegalStateException("Can not forfeit an already finished game!")
 }
 
 /**
@@ -150,6 +154,7 @@ class TicTacToeBoardWin(
  * @param player1 the first player.
  * @param player2 the second player.
  * @return TicTacToeBoard the Tic Tac Toe board in a draw state with the information.
+ * @return BoardDraw the Tic Tac Toe board in a draw state with the information.
  */
 class TicTacToeBoardDraw(
     override val positions: List<TicPosition>,
@@ -161,28 +166,22 @@ class TicTacToeBoardDraw(
 
     /**
      * Function play responsible to play a move on the board.
-     * @param player the player that is playing.
-     * @param row the row of the move.
-     * @param column the column of the move.
+     * @param move the move to be done in this play.
      * @return Board the board after the move was played.
      */
-    override fun play(move: Move): Board {
-        throw IllegalStateException("The game has already ended on a draw.")
-    }
+    override fun play(move: Move): Board = throw IllegalStateException("The game has already ended on a draw.")
 
     /**
      * Function forfeit responsible to forfeit the game board.
      * @param player the player that is forfeiting.
      * @return Board the game after the player forfeited.
      */
-    override fun forfeit(player: Player): Board {
-        throw IllegalStateException("Can not forfeit an already finished game!")
-    }
+    override fun forfeit(player: Player): Board = throw IllegalStateException("Can not forfeit an already finished game!")
 }
 
 /**
  * Function initialTicTacToePositions responsible to create the initial positions of the Tic Tac Toe board.
- * @return List<Position> the list of initial positions.
+ * @return List<TicPosition> the list of initial positions.
  */
 fun initialTicTacToePositions():List<TicPosition> {
     val positions = mutableListOf<TicPosition>()
@@ -201,4 +200,3 @@ fun initialTicTacToePositions():List<TicPosition> {
     }
     return positions
 }
-
--- code/CrossBoard Application/shared/src/commonTest/kotlin/TicTacToeBoardTest.kt
+++ code/CrossBoard Application/shared/src/commonTest/kotlin/TicTacToeBoardTest.kt
@@ -1,9 +1,415 @@
-import com.crossBoard.domain.initialTicTacToePositions
+import com.crossBoard.domain.*
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
 
 class TicTacToeBoardTest {
-    @Test fun `Test initial positions`(){
+    @Test fun creatingTicTacToeBoardRun() {
+
+        val positions = emptyList<TicPosition>()
+        val moves = emptyList<TicTacToeMove>()
+        val player1 = Player.BLACK
+        val player2 = Player.WHITE
+
+        val board = TicTacToeBoardRun(positions, moves, player1, player1, player2)
+
+        assertTrue(board is TicTacToeBoard)
+        assertTrue(board is BoardRun)
+
+        assertEquals(moves, board.moves)
+        assertEquals(positions, board.positions)
+        assertEquals(player1, board.player1)
+        assertEquals(player2, board.player2)
+        assertEquals(player1, board.turn)
+    }
+
+    @Test fun playingTicTacToeBoardRunWithoutSuccess() {
+
         val positions = initialTicTacToePositions()
-        println(positions)
+        val moves = emptyList<TicTacToeMove>()
+        val player1 = Player.BLACK
+        val player2 = Player.WHITE
+
+        val board = TicTacToeBoardRun(positions, moves, player1, player1, player2)
+
+        assertTrue(board is TicTacToeBoard)
+        assertTrue(board is BoardRun)
+
+        assertFailsWith<IllegalArgumentException> {
+            val move = TicTacToeMove(player2, Square(Row(0, 3), Column('a')))
+            board.play(move)
+        }
+
+        assertFailsWith<IllegalArgumentException> {
+            val move1 = TicTacToeMove(player1, Square(Row(0, 3), Column('a')))
+            val newBoard = board.play(move1)
+            val move2 = TicTacToeMove(player2, Square(Row(0, 3), Column('a')))
+            newBoard.play(move2)
+        }
+    }
+
+    @Test fun playingTicTacToeBoardRunWithSuccess() {
+        val positions = initialTicTacToePositions()
+        val moves = emptyList<TicTacToeMove>()
+        val player1 = Player.BLACK
+        val player2 = Player.WHITE
+
+        val board = TicTacToeBoardRun(positions, moves, player1, player1, player2)
+
+        assertTrue(board is TicTacToeBoard)
+        assertTrue(board is BoardRun)
+
+        val move = TicTacToeMove(player1, Square(Row(0, 3), Column('a')))
+        val finalBoard = board.play(move)
+
+        assertTrue(finalBoard is TicTacToeBoardRun)
+        assertTrue(finalBoard is BoardRun)
+
+        val finalPositions = listOf<TicPosition>(
+            TicPosition(player1, Square(Row(0, 3), Column('a'))),
+            TicPosition(Player.EMPTY, Square(Row(0, 3), Column('b'))),
+            TicPosition(Player.EMPTY, Square(Row(0, 3), Column('c'))),
+            TicPosition(Player.EMPTY, Square(Row(1, 3), Column('a'))),
+            TicPosition(Player.EMPTY, Square(Row(1, 3), Column('b'))),
+            TicPosition(Player.EMPTY, Square(Row(1, 3), Column('c'))),
+            TicPosition(Player.EMPTY, Square(Row(2, 3), Column('a'))),
+            TicPosition(Player.EMPTY, Square(Row(2, 3), Column('b'))),
+            TicPosition(Player.EMPTY, Square(Row(2, 3), Column('c')))
+        )
+
+        finalPositions.forEachIndexed { index, ticPosition ->
+            assertEquals(ticPosition.player, finalBoard.positions[index].player)
+            assertEquals(ticPosition.square.row.number, finalBoard.positions[index].square.row.number)
+            assertEquals(ticPosition.square.column.symbol, finalBoard.positions[index].square.column.symbol)
+        }
+
+        val finalMoves = listOf<TicTacToeMove>(
+            TicTacToeMove(player1, Square(Row(0, 3), Column('a')))
+        )
+
+        assertEquals(finalMoves, finalBoard.moves)
+
+        assertEquals(player2, finalBoard.turn)
+        assertEquals(player1, finalBoard.player1)
+        assertEquals(player2, finalBoard.player2)
+    }
+
+    @Test fun playingTicTacToeBoardRunToWin() {
+        val positions = initialTicTacToePositions()
+        val moves = emptyList<TicTacToeMove>()
+        val player1 = Player.BLACK
+        val player2 = Player.WHITE
+
+        val board = TicTacToeBoardRun(positions, moves, player1, player1, player2)
+
+        assertTrue(board is TicTacToeBoard)
+        assertTrue(board is BoardRun)
+
+        val move1 = TicTacToeMove(player1, Square(Row(0, 3), Column('a')))
+        val board1 = board.play(move1)
+        val move2 = TicTacToeMove(player2, Square(Row(1, 3), Column('a')))
+        val board2 = board1.play(move2)
+        val move3 = TicTacToeMove(player1, Square(Row(0, 3), Column('b')))
+        val board3 = board2.play(move3)
+        val move4 = TicTacToeMove(player2, Square(Row(1, 3), Column('b')))
+        val board4 = board3.play(move4)
+        val move5 = TicTacToeMove(player1, Square(Row(0, 3), Column('c')))
+        val finalBoard = board4.play(move5)
+
+        assertTrue(finalBoard is TicTacToeBoardWin)
+        assertTrue(finalBoard is BoardWin)
+
+        val finalPositions: List<TicPosition> = listOf<TicPosition>(
+            TicPosition(player1, Square(Row(0, 3), Column('a'))),
+            TicPosition(player1, Square(Row(0, 3), Column('b'))), TicPosition(player1, Square(Row(0, 3), Column('c'))),
+            TicPosition(player2, Square(Row(1, 3), Column('a'))), TicPosition(player2, Square(Row(1, 3), Column('b'))),
+            TicPosition(Player.EMPTY, Square(Row(1, 3), Column('c'))), TicPosition(Player.EMPTY, Square(Row(2, 3), Column('a'))),
+            TicPosition(Player.EMPTY, Square(Row(2, 3), Column('b'))), TicPosition(Player.EMPTY, Square(Row(2, 3), Column('c')))
+        )
+
+        finalPositions.forEachIndexed { index, ticPosition ->
+            assertEquals(ticPosition.player, finalBoard.positions[index].player)
+            assertEquals(ticPosition.square.row.number, finalBoard.positions[index].square.row.number)
+            assertEquals(ticPosition.square.column.symbol, finalBoard.positions[index].square.column.symbol)
+        }
+
+        val finalMoves = listOf<TicTacToeMove>(
+            TicTacToeMove(player1, Square(Row(0, 3), Column('a'))),
+            TicTacToeMove(player2, Square(Row(1, 3), Column('a'))),
+            TicTacToeMove(player1, Square(Row(0, 3), Column('b'))),
+            TicTacToeMove(player2, Square(Row(1, 3), Column('b'))),
+            TicTacToeMove(player1, Square(Row(0, 3), Column('c')))
+        )
+
+        assertEquals(finalMoves, finalBoard.moves)
+
+        assertEquals(player1, finalBoard.winner)
+        assertEquals(player2, finalBoard.turn)
+        assertEquals(player1, finalBoard.player1)
+        assertEquals(player2, finalBoard.player2)
+    }
+
+    @Test fun playingTicTacToeBoardRunToDraw() {
+        val positions = initialTicTacToePositions()
+        val moves = emptyList<TicTacToeMove>()
+        val player1 = Player.BLACK
+        val player2 = Player.WHITE
+
+        val board = TicTacToeBoardRun(positions, moves, player1, player1, player2)
+
+        assertTrue(board is TicTacToeBoard)
+        assertTrue(board is BoardRun)
+
+        val move1 = TicTacToeMove(player1, Square(Row(0, 3), Column('a')))
+        val board1 = board.play(move1)
+        val move2 = TicTacToeMove(player2, Square(Row(1, 3), Column('a')))
+        val board2 = board1.play(move2)
+        val move3 = TicTacToeMove(player1, Square(Row(0, 3), Column('b')))
+        val board3 = board2.play(move3)
+        val move4 = TicTacToeMove(player2, Square(Row(1, 3), Column('b')))
+        val board4 = board3.play(move4)
+        val move5 = TicTacToeMove(player1, Square(Row(1, 3), Column('c')))
+        val board5 = board4.play(move5)
+        val move6 = TicTacToeMove(player2, Square(Row(0, 3), Column('c')))
+        val board6 = board5.play(move6)
+        val move7 = TicTacToeMove(player1, Square(Row(2, 3), Column('a')))
+        val board7 = board6.play(move7)
+        val move8 = TicTacToeMove(player2, Square(Row(2, 3), Column('b')))
+        val board8 = board7.play(move8)
+        val move9 = TicTacToeMove(player1, Square(Row(2, 3), Column('c')))
+        val finalBoard = board8.play(move9)
+
+        assertTrue(finalBoard is TicTacToeBoardDraw)
+        assertTrue(finalBoard is BoardDraw)
+
+        val finalPositions: List<TicPosition> = listOf<TicPosition>(
+            TicPosition(player1, Square(Row(0, 3), Column('a'))),
+            TicPosition(player1, Square(Row(0, 3), Column('b'))), TicPosition(player2, Square(Row(0, 3), Column('c'))),
+            TicPosition(player2, Square(Row(1, 3), Column('a'))), TicPosition(player2, Square(Row(1, 3), Column('b'))),
+            TicPosition(player1, Square(Row(1, 3), Column('c'))), TicPosition(player1, Square(Row(2, 3), Column('a'))),
+            TicPosition(player2, Square(Row(2, 3), Column('b'))), TicPosition(player1, Square(Row(2, 3), Column('c')))
+        )
+
+        finalPositions.forEachIndexed { index, ticPosition ->
+            assertEquals(ticPosition.player, finalBoard.positions[index].player)
+            assertEquals(ticPosition.square.row.number, finalBoard.positions[index].square.row.number)
+            assertEquals(ticPosition.square.column.symbol, finalBoard.positions[index].square.column.symbol)
+        }
+
+        val finalMoves = listOf<TicTacToeMove>(
+            TicTacToeMove(player1, Square(Row(0, 3), Column('a'))),
+            TicTacToeMove(player2, Square(Row(1, 3), Column('a'))),
+            TicTacToeMove(player1, Square(Row(0, 3), Column('b'))),
+            TicTacToeMove(player2, Square(Row(1, 3), Column('b'))),
+            TicTacToeMove(player1, Square(Row(1, 3), Column('c'))),
+            TicTacToeMove(player2, Square(Row(0, 3), Column('c'))),
+            TicTacToeMove(player1, Square(Row(2, 3), Column('a'))),
+            TicTacToeMove(player2, Square(Row(2, 3), Column('b'))),
+            TicTacToeMove(player1, Square(Row(2, 3), Column('c')))
+        )
+
+        assertEquals(finalMoves, finalBoard.moves)
+
+        assertEquals(player2, finalBoard.turn)
+        assertEquals(player1, finalBoard.player1)
+        assertEquals(player2, finalBoard.player2)
+    }
+
+    @Test fun forfeitingTicTacToeBoardRun() {
+        val positions = initialTicTacToePositions()
+        val moves = emptyList<TicTacToeMove>()
+        val player1 = Player.BLACK
+        val player2 = Player.WHITE
+
+        val board = TicTacToeBoardRun(positions, moves, player1, player1, player2)
+
+        assertTrue(board is TicTacToeBoard)
+        assertTrue(board is BoardRun)
+
+        val move = TicTacToeMove(player1, Square(Row(0, 3), Column('a')))
+        val newBoard = board.play(move)
+
+        assertTrue(newBoard is TicTacToeBoardRun)
+        assertTrue(newBoard is BoardRun)
+
+        val finalBoard = newBoard.forfeit(player1)
+
+        assertTrue(finalBoard is TicTacToeBoardWin)
+        assertTrue(finalBoard is BoardWin)
+
+        assertEquals(player2, finalBoard.winner)
+    }
+
+    @Test fun verifyWinnerTicTacToeBoardWin() {
+        val positions = initialTicTacToePositions()
+        val moves = emptyList<TicTacToeMove>()
+        val player1 = Player.BLACK
+        val player2 = Player.WHITE
+
+        val board = TicTacToeBoardRun(positions, moves, player1, player1, player2)
+
+        assertTrue(board is TicTacToeBoard)
+        assertTrue(board is BoardRun)
+
+        val move1 = TicTacToeMove(player1, Square(Row(0, 3), Column('a')))
+        val board1 = board.play(move1)
+        val move2 = TicTacToeMove(player2, Square(Row(1, 3), Column('a')))
+        val board2 = board1.play(move2)
+        val move3 = TicTacToeMove(player1, Square(Row(0, 3), Column('b')))
+        val board3 = board2.play(move3)
+        val move4 = TicTacToeMove(player2, Square(Row(1, 3), Column('b')))
+        val board4 = board3.play(move4)
+        val move5 = TicTacToeMove(player1, Square(Row(0, 3), Column('c')))
+        val finalBoard1 = board4.play(move5)
+
+        assertTrue(finalBoard1 is TicTacToeBoardWin)
+        assertTrue(finalBoard1 is BoardWin)
+        assertEquals(player1, finalBoard1.winner)
+
+        val move6 = TicTacToeMove(player1, Square(Row(0, 3), Column('a')))
+        val board6 = board.play(move6)
+        val move7 = TicTacToeMove(player2, Square(Row(0, 3), Column('b')))
+        val board7 = board6.play(move7)
+        val move8 = TicTacToeMove(player1, Square(Row(1, 3), Column('a')))
+        val board8 = board7.play(move8)
+        val move9 = TicTacToeMove(player2, Square(Row(1, 3), Column('b')))
+        val board9 = board8.play(move9)
+        val move10 = TicTacToeMove(player1, Square(Row(2, 3), Column('a')))
+        val finalBoard2 = board9.play(move10)
+
+        assertTrue(finalBoard2 is TicTacToeBoardWin)
+        assertTrue(finalBoard2 is BoardWin)
+        assertEquals(player1, finalBoard2.winner)
+
+        val move11 = TicTacToeMove(player1, Square(Row(0, 3), Column('a')))
+        val board11 = board.play(move11)
+        val move12 = TicTacToeMove(player2, Square(Row(0, 3), Column('b')))
+        val board12 = board11.play(move12)
+        val move13 = TicTacToeMove(player1, Square(Row(1, 3), Column('b')))
+        val board13 = board12.play(move13)
+        val move14 = TicTacToeMove(player2, Square(Row(1, 3), Column('c')))
+        val board14 = board13.play(move14)
+        val move15 = TicTacToeMove(player1, Square(Row(2, 3), Column('c')))
+        val finalBoard3 = board14.play(move15)
+
+        assertTrue(finalBoard3 is TicTacToeBoardWin)
+        assertTrue(finalBoard3 is BoardWin)
+        assertEquals(player1, finalBoard3.winner)
+    }
+
+    @Test fun createTicTacToeBoardWin() {
+        val positions = initialTicTacToePositions()
+        val moves = emptyList<TicTacToeMove>()
+        val player1 = Player.BLACK
+        val player2 = Player.WHITE
+
+        val board = TicTacToeBoardWin(player1, positions, moves, player1, player1, player2)
+
+        assertTrue(board is TicTacToeBoard)
+        assertTrue(board is BoardWin)
+
+        assertEquals(player1, board.winner)
+        assertEquals(moves, board.moves)
+        assertEquals(positions, board.positions)
+        assertEquals(player1, board.player1)
+        assertEquals(player2, board.player2)
+        assertEquals(player1, board.turn)
+    }
+
+    @Test fun playTicTacToeBoardWin() {
+        val positions = initialTicTacToePositions()
+        val moves = emptyList<TicTacToeMove>()
+        val player1 = Player.BLACK
+        val player2 = Player.WHITE
+
+        val board = TicTacToeBoardWin(player1, positions, moves, player1, player1, player2)
+
+        assertTrue(board is TicTacToeBoard)
+        assertTrue(board is BoardWin)
+
+        assertFailsWith<IllegalStateException> {
+            val move = TicTacToeMove(player2, Square(Row(0, 3), Column('a')))
+            board.play(move)
+        }
+    }
+
+    @Test fun forfeitTicTacToeBoardWin() {
+        val positions = initialTicTacToePositions()
+        val moves = emptyList<TicTacToeMove>()
+        val player1 = Player.BLACK
+        val player2 = Player.WHITE
+
+        val board = TicTacToeBoardWin(player1, positions, moves, player1, player1, player2)
+
+        assertTrue(board is TicTacToeBoard)
+        assertTrue(board is BoardWin)
+
+        assertFailsWith<IllegalStateException> {
+            board.forfeit(player2)
+        }
+    }
+
+    @Test fun createTicTacToeBoardDraw() {
+        val positions = initialTicTacToePositions()
+        val moves = emptyList<TicTacToeMove>()
+        val player1 = Player.BLACK
+        val player2 = Player.WHITE
+
+        val board = TicTacToeBoardDraw(positions, moves, player1, player1, player2)
+
+        assertTrue(board is TicTacToeBoard)
+        assertTrue(board is BoardDraw)
+
+        assertEquals(moves, board.moves)
+        assertEquals(positions, board.positions)
+        assertEquals(player1, board.player1)
+        assertEquals(player2, board.player2)
+        assertEquals(player1, board.turn)
+    }
+
+    @Test fun playTicTacToeBoardDraw() {
+        val positions = initialTicTacToePositions()
+        val moves = emptyList<TicTacToeMove>()
+        val player1 = Player.BLACK
+        val player2 = Player.WHITE
+
+        val board = TicTacToeBoardDraw(positions, moves, player1, player1, player2)
+
+        assertTrue(board is TicTacToeBoard)
+        assertTrue(board is BoardDraw)
+
+        assertFailsWith<IllegalStateException> {
+            val move = TicTacToeMove(player1, Square(Row(0, 3), Column('a')))
+            board.play(move)
+        }
+    }
+
+    @Test fun forfeitTicTacToeBoardDraw() {
+        val positions = initialTicTacToePositions()
+        val moves = emptyList<TicTacToeMove>()
+        val player1 = Player.BLACK
+        val player2 = Player.WHITE
+
+        val board = TicTacToeBoardDraw(positions, moves, player1, player1, player2)
+
+        assertTrue(board is TicTacToeBoard)
+        assertTrue(board is BoardDraw)
+
+        assertFailsWith<IllegalStateException> {
+            board.forfeit(player2)
+        }
+    }
+
+    @Test fun initialTicTacToePositionsTest() {
+        val positions = initialTicTacToePositions()
+
+        assertEquals(9, positions.size)
+
+        positions.forEachIndexed { index, position ->
+            assertEquals(Player.EMPTY, position.player)
+            assertEquals(Row(index / TicTacToeBoard.BOARD_DIM, TicTacToeBoard.BOARD_DIM).number, position.square.row.number)
+            assertEquals(Column('a' + index % TicTacToeBoard.BOARD_DIM).symbol, position.square.column.symbol)
+        }
     }
 }
